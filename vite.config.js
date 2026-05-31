28919bebd35c61469b1b41387dc87b43483b9075
NisargPatel03 <kbnisargpatel001454@gmail.com>
2026-05-31 19:19:34 +0530

    Fix Console Error

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,21 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  optimizeDeps: {
+    include: ['recharts'],
+  },
   build: {
-    minify: false,
-  }
+    commonjsOptions: {
+      transformMixedEsModules: true,
+    },
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules/recharts') || id.includes('node_modules/es-toolkit')) {
+            return 'recharts-vendor';
+          }
+        },
+      },
+    },
+  },
 })
